7658498a820746b5bb24012ddd8b71f452bb0075
bigbangs2020 <109906687+bigbangs2020@users.noreply.github.com>
2022-07-31 22:47:04 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+console.log('Abanga Samuel Beshel ')
+console.log('Height: 175cm');
+console.log('Country: Nigeria');
